7221235fe01a7cf63921941f4638e0376fdf96a2
osawa-koki <github@osawa.dev>
2023-01-10 04:05:24 +0900

    コードスタイル調整。

--- normal/main.go
+++ normal/main.go
@@ -8,12 +8,14 @@ import (
 	"os"
 )
 
-func main() {
-	const (
-		xmin, ymin, xmax, ymax = -2, -2, +2, +2
-		width, height          = 1024, 1024
-	)
+const (
+	xmin, ymin, xmax, ymax = -2, -2, +2, +2
+	width, height          = 1024, 1024
+	iterations             = 200
+	contrast               = 15
+)
 
+func main() {
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	for py := 0; py < height; py++ {
 		y := float64(py)/height*(ymax-ymin) + ymin
@@ -30,9 +32,6 @@ func main() {
 }
 
 func julia(z complex128) color.Color {
-	const iterations = 200
-	const contrast = 15
-
 	var v complex128 = complex(-0.8, 0.156)
 	for n := uint8(0); n < iterations; n++ {
 		z = z*z + v
